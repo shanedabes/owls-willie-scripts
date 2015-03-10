6f06b96bdd4fe551def00c0167394b1c5909a8ec
sharktamer <donohoe.shane@gmail.com>
2015-03-10 01:37:38 +0000

    Add test to hello module

--- hello.py
+++ hello.py
@@ -6,9 +6,14 @@ insultees = ["SebK", "`Edd"]
 
 
 @module.nickname_commands('hello')
+@module.example('{} hello'.format(bot.nick), 'Hi {}'.format(trigger.nick))
 def hi(bot, trigger):
     if trigger.nick in insultees:
         bot.say("Shut up " + choice(meanWords))
     else:
         bot.say('Hi, ' + trigger.nick)
+
+if __name__ == '__main__':
+    from willie.test_tools import run_example_tests
+    run_example_tests(__file__)
 
