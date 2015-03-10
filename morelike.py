777b71dff605bdd0840cfa2e44fe5a2a239dfab5
sharktamer <donohoe.shane@gmail.com>
2015-03-10 11:57:07 +0000

    clean import, add blank line at end

--- morelike.py
+++ morelike.py
@@ -1,9 +1,10 @@
-from willie import module
+from willie.module import rule
 import re
 
 
-@module.rule(r"(.*ay.*)")
+@rule(r"(.*ay.*)")
 def morelike(bot, trigger):
     msg = trigger.group(1)
     msg = re.sub(r"[^aeiou ]+ay", "GAY", msg)
     bot.say("More like " + msg)
+
