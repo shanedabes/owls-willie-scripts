d734790c0348dea9fd44e80fbe91d33d54eb6406
sharktamer <donohoe.shane@gmail.com>
2017-02-18 18:54:27 +0000

    remove duplicate colours, use cycle to prevent same colour pairs

--- checkiday.py
+++ checkiday.py
@@ -5,13 +5,15 @@ from sopel.formatting import color
 import requests
 import re
 import random
+from itertools import cycle
 
 
 @commands('checkiday')
 def checkiday(bot, trigger):
     r = requests.get('https://www.checkiday.com/rss.php?tz=Europe/London')
     days = re.findall(r'<title>(.*?)</title>', r.text)[1:]
-    colors = random.sample(range(2, 16), len(days))
+
+    colors = cycle(random.sample([2, 3, 4, 6, 7, 14, 15], 7))
     cdays = [color(i, j) + u'\x0f' for i, j in zip(days, colors)]
     out = 'Today is {}'.format(', '.join(cdays))
     bot.say(out)
