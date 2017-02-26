6a6cbe69ac7d8c47c5a1689a46705034d84b92f6
sharktamer <donohoe.shane@gmail.com>
2017-02-26 14:05:00 +0000

    new superpower script

---
+++ superpower.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import requests
+import re
+from sopel.module import commands
+
+
+@commands('superpower')
+def superpower(bot, trigger):
+    r = requests.get('http://powerlisting.wikia.com/wiki/Special:Random')
+    name = re.search('<h1>.*?</h1>', r.text).group()
+    desc = re.search('<p>.*', r.text).group()
+
+    name, desc = [re.sub(r'<.*?>', '', i) for i in (name, desc)]
+    out = '{}: {} ({})'.format(name, desc, r.url)
+
+    bot.say(out)
