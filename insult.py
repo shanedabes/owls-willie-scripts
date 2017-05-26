bd58bf7bd38d5a712acff0c2a630767374f08740
sharktamer <donohoe.shane@gmail.com>
2017-05-26 16:58:53 +0100

    add new insult service

--- insult.py
+++ insult.py
@@ -3,10 +3,14 @@
 from sopel.module import commands
 import requests
 import re
+from random import choice
 
-api = 'http://www.insultgenerator.org/'
-nice_api = 'http://toykeeper.net/programs/mad/compliments'
-nice_people = ('sharktamer', 'shane', 'donohoe', 'darkjesus', 'princess', 'declan')
+api = 'http://www.dickless.org/api/insult.xml'
+nice_api = ('https://spreadsheets.google.com/feeds/list/'
+            '1eEa2ra2yHBXVZ_ctH4J15tFSGEu-VTSunsrvaCAV598/'
+            'od6/public/values?alt=json')
+nice_people = ('sharktamer', 'shane', 'donohoe', 'darkjesus', 'princess',
+               'declan')
 
 
 @commands('insult')
@@ -15,13 +19,14 @@ def insult(bot, trigger):
 
     if any([n in user.lower() for n in nice_people]):
         r = requests.get(nice_api)
-        #thread = re.search(r'(?:class="threadtitle"[^>]+>)([^<]+)', r.content)
-        msg = re.search(r'(?<=class="blurb_title_1">)[^<]+', r.text).group(0)
-        #msg = re.search('(?<=<br><br>)[^<]+', r.content).group(0)
-        #print thread, post
+        compliments = [i['title']['$t'] for i in r.json()['feed']['entry']]
+        msg = choice(compliments)
+        # thread = re.search(r'(?:class="threadtitle"[^>]+>)([^<]+)', r.content)
+        # msg = re.search(r'(?<=class="blurb_title_1">)[^<]+', r.text).group(0)
+        # msg = re.search('(?<=<br><br>)[^<]+', r.content).group(0)
+        # print thread, post
     else:
         r = requests.get(api)
-        msg = re.search(r'(?<=<br><br>)[^<]+', r.text).group(0)
+        msg = re.search(r'<insult>(.*?)</insult>', r.text).groups()[0]
 
     bot.say('{0}: {1}'.format(user, msg))
-
