21c12def8e5aeb11d0dbbd7d5f01e026816f198a
sharktamer <donohoe.shane@gmail.com>
2017-06-02 11:26:09 +0000

    add reply module

--- congratulations.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-from sopel.module import rule
-
-
-@rule(r'^[Cc]ongratulations!?')
-def congratulations(bot, trigger):
-    bot.say('congratulations!')
---
+++ reply.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+from sopel.config.types import StaticSection, ListAttribute
+from sopel.config import ConfigurationError
+from sopel.modules import reload
+from sopel.module import commands
+
+
+class ReplySection(StaticSection):
+    rules = ListAttribute('rules')
+    replies = ListAttribute('replies')
+
+
+def setup(bot):
+    bot.config.define_section('reply', ReplySection)
+
+    reply.rule = bot.config.reply.rules
+
+    if len(bot.config.reply.rules) != len(bot.config.reply.replies):
+        raise ConfigurationError('Number of rules and replies must match')
+
+
+def configure(config):
+    config.define_section('reply', ReplySection, validate=False)
+    config.reply.configure_setting('rules', 'enter rules to listen for')
+    config.reply.configure_setting('replies', 'enter corresponding replies')
+
+
+def reply(bot, trigger):
+    rules, replies = bot.config.reply.rules, bot.config.reply.replies
+    rule = trigger.args[1]
+    reply = replies[rules.index(rule)]
+
+    bot.say(reply)
