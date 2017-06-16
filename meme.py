fe4157afc6a2aaf1d4614916243baae544ba15b6
sharktamer <donohoe.shane@gmail.com>
2017-06-16 14:29:50 +0000

    check for meme, give list

--- meme.py
+++ meme.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from sopel.module import commands
-# import requests
+import requests
 
 
 def escape(text):
@@ -17,9 +17,16 @@ def escape(text):
 def meme(bot, trigger):
     if not trigger.group(2) or trigger.group(2).count('/') < 2:
         bot.say('Usage: .meme meme_type/top_text/bottom_text')
+        bot.say('List of meme templates: https://memegen.link/api/templates/')
         return
 
     meme, top, bottom = [escape(i) for i in trigger.group(2).split('/', 2)]
+
+    check = requests.get('https://memegen.link/api/templates/{}'.format(meme))
+    if check.status_code != 200:
+        bot.say('Meme not recognised')
+        bot.say('List of meme templates: https://memegen.link/api/templates/')
+        return
 
     url = 'https://memegen.link/{}/{}/{}.jpg'.format(meme, top, bottom)
 
