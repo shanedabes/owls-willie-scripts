47c64303c84b0f8d2d084b0585e2409cedc0780b
sharktamer <donohoe.shane@gmail.com>
2017-07-03 14:32:47 +0000

    compile regex

--- dailyzen.py
+++ dailyzen.py
@@ -8,7 +8,8 @@ import re
 def zen(bot, trigger):
     r = requests.get('https://www.dailyzen.com/')
 
-    zr = re.search(r'<blockquote>(.*)</blockquote>', r.text.replace('\n', ' '))
+    regex = re.compile(r'<blockquote>(.*)</blockquote>')
+    zr = regex.search(r.text.replace('\n', ' '))
     z = ' '.join(zr.group(1).split()).split('<BR>')
 
     cr = re.search(r'<cite>(.*)</cite>', r.text.replace('\n', ' '))
