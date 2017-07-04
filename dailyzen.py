9af4dd1406b2dcfa1773ef8fe2b873a11f43a43a
sharktamer <donohoe.shane@gmail.com>
2017-07-04 13:26:56 +0000

    remove p tags and unescape

--- dailyzen.py
+++ dailyzen.py
@@ -3,18 +3,20 @@
 from sopel.module import commands
 import requests
 import re
+from html import unescape
+
 
 @commands('zen')
 def zen(bot, trigger):
     r = requests.get('https://www.dailyzen.com/')
 
-    regex = re.compile(r'<blockquote>(.*)</blockquote>')
-    zr = regex.search(r.text.replace('\n', ' '))
+    zr = re.search(r'<blockquote>\s*(?:<p>)?(.*?)(?:</p>)?\s*</blockquote>',
+                   r.text.replace('\n', ' '))
     z = ' '.join(zr.group(1).split()).split('<BR>')
 
     cr = re.search(r'<cite>(.*)</cite>', r.text.replace('\n', ' '))
     c = ' '.join(cr.group(1).split())
 
     for line in z:
-        bot.say(line.strip())
+        bot.say(unescape(line.strip()))
     bot.say(c.strip())
