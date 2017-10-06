dc0647d476f22a75351f1f8764f8c8b9cc563bd3
sharktamer <donohoe.shane@gmail.com>
2017-10-06 16:01:16 +0100

    fix superpower script

--- superpower.py
+++ superpower.py
@@ -8,7 +8,7 @@ from sopel.module import commands
 @commands('superpower')
 def superpower(bot, trigger):
     r = requests.get('http://powerlisting.wikia.com/wiki/Special:Random')
-    name = re.search('<h1>.*?</h1>', r.text).group()
+    name = re.search('<h1.*>.*?</h1>', r.text).group()
     desc = re.search('<p>.*', r.text).group()
 
     name, desc = [re.sub(r'<.*?>', '', i) for i in (name, desc)]
